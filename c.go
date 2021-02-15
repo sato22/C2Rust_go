7a1ae7bef9386b924a9d5fc87dcf0a828299808b
sato <53112714+sato22@users.noreply.github.com>
2021-02-15 10:50:35 +0900

    Update c.go

--- c.go
+++ c.go
@@ -4,38 +4,29 @@ import (
 // 	"fmt"
 )
 
-type CVar struct {
-	label   string
-	vartype string
-}
-
 type CVarDecl struct {
-	cvar        CVar
+	label       string
+	vartype     string
 	initializer string // this should be an expr?
+	dim         int
+	size        []string
 }
 
 type CAssignment struct {
 	label    string
+	size     []string
 	right    string // this should be an expr?
 	operator string
 }
 
-type CArray struct {
-	dim         int      // arraydim exitdirectdeclarator で case:3の際に++
-	size        []string // a[3][4] の数字部分
-	label       string   // arraylabel
-	arraytype   string
-	initializer string
-}
-
 type CFunction struct {
 	label      string
-	args       []CVar
+	args       []CVarDecl
 	body       []CStatement
 	returntype string
 }
 
 type CStatement interface {
 	UpdateEnv(*Env) *Env
-	AddStatement(*Env, []RWriter) []RWriter
+	AddStatement(*Env, []RWriter, []RVarDecl) []RWriter
 }
